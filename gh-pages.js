6cfbaa52f3a4961e8593d4a094de2158e5680536
Anthony2be <adubovitsky1234@gmail.com>
2022-06-14 15:37:12 -0700

    test

---
+++ gh-pages.js
@@ -0,0 +1,16 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish(
+    'public', // path to public directory
+    {
+        branch: 'gh-pages',
+        repo: 'https://github.com/Anthony2be/Anthony2be.github.io.git', // Update to point to your repository  
+        user: {
+            name: 'Anthony2be', // update to use your name
+            email: 'adubovitsky1234@gmail.com' // Update to use your email
+        }
+    },
+    () => {
+        console.log('Deploy Complete!')
+    }
+)
