cc0cd54c0fef34d3f2a55f172559716565c792ef
Anthony2be <adubovitsky1234@gmail.com>
2022-06-14 23:49:12 -0700

    made it dynamicly resize :0

--- src/lib/Friends.svelte
+++ src/lib/Friends.svelte
@@ -4,17 +4,26 @@
     export let username;
 
     let friends = []
-    
+    let width = 100;
+    let innerWidth = window.innerWidth;
+
+    function onResize(){
+        width = (window.innerWidth - (friends.length * 50))/friends.length;
+    }
+            
     onMount(async () => {
         let data = await fetch(`https://api.github.com/users/${username}/following`)
             .then(res => res.json())
         friends = data;
+        width = (innerWidth - (friends.length * 50))/friends.length;
     });
+
+    window.addEventListener('resize', onResize);
 </script>
 <div>
     {#each friends as d}
         <a href={d.html_url}>
-            <img src={d.avatar_url} alt="icon" width="100" height="100">
+            <img src={d.avatar_url} alt="icon" {width} height={width}>
         </a>
     {/each}
 </div>
